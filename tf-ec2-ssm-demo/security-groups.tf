9889793b1a39601d78853ba5202a5c2de22b1d45
Julio Arana Jr <julio.arana@cloudreach.com>
2023-02-04 14:53:47 +0100

    update ec2 ssm files

--- tf-ec2-ssm-demo/security-groups.tf
+++ tf-ec2-ssm-demo/security-groups.tf
@@ -4,10 +4,10 @@ resource "aws_security_group" "server" {
   vpc_id      = data.aws_vpc.talent_academy.id
 
   egress {
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
